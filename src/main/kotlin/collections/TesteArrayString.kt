26957836c2b4242c957c4df7aa116a56d3ba8afd
Cleide <cleide.fiscal@gmail.com>
2022-07-10 08:46:32 -0300

    created string array of test class

---
+++ src/main/kotlin/collections/TesteArrayString.kt
@@ -0,0 +1,21 @@
+package collections
+
+fun main(){
+    val nomes = Array(3) {""}
+    nomes[0] = "Maria"
+    nomes[1] = "Zaza"
+    nomes[2] = "Jose"
+
+    for (nome in nomes) {
+        println(nomes)
+    }
+
+    println("---------------------")
+    nomes.sort()
+    nomes.forEach { println(it) }
+
+    println("---------------------")
+    val nomes2 = arrayOf("Maria", "Zaza", "Pedro")
+    nomes2.sort()
+    nomes2.forEach { println(it) }
+}
